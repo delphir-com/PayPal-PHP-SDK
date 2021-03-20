7b5da8c32e38c3780e754204f3221112b95d10a2
Konstantin T <kt@delphir.com>
2021-03-20 04:22:42 +0200

    add app context properties

--- lib/PayPal/Api/ApplicationContext.php
+++ lib/PayPal/Api/ApplicationContext.php
@@ -12,12 +12,83 @@ use PayPal\Validation\UrlValidator;
  *
  * @package PayPal\Api
  *
+ * @property string brand_name
+ * @property string locale
+ * @property string shipping_preference
  * @property string cancel_url
  * @property string return_url
  */
 
 class ApplicationContext extends PayPalResourceModel
 {
+    const SHIPPING_PREF__SET_PROVIDED_ADDRESS = 'SET_PROVIDED_ADDRESS';
+    const SHIPPING_PREF__NO_SHIPPING          = 'NO_SHIPPING';
+
+    /**
+     * Sets the BrandName
+     *
+     * @param string $brand_name
+     * @return $this
+     */
+    public function setBrandName($brand_name)
+    {
+        $this->brand_name = $brand_name;
+        return $this;
+    }
+
+    /**
+     * Get the BrandName
+     *
+     * @return string
+     */
+    public function getBrandName()
+    {
+        return $this->brand_name;
+    }
+
+    /**
+     * Sets the Locale
+     *
+     * @param string $locale
+     * @return $this
+     */
+    public function setLocale($locale)
+    {
+        $this->locale = $locale;
+        return $this;
+    }
+
+    /**
+     * Get the Locale
+     *
+     * @return string
+     */
+    public function getLocale()
+    {
+        return $this->locale;
+    }
+
+    /**
+     * Sets the ShippingPreference
+     *
+     * @param string $shipping_preference
+     * @return $this
+     */
+    public function setShippingPreference($shipping_preference)
+    {
+        $this->shipping_preference = $shipping_preference;
+        return $this;
+    }
+
+    /**
+     * Get the ShippingPreference
+     *
+     * @return string
+     */
+    public function getShippingPreference()
+    {
+        return $this->shipping_preference;
+    }
 
     /**
      * Redirect URL on cancellation of agreement request. 1000 characters max.
@@ -42,7 +113,6 @@ class ApplicationContext extends PayPalResourceModel
     {
         return $this->cancel_url;
     }
-
 
     /**
      * Redirect URL on creation of agreement request. 1000 characters max.
